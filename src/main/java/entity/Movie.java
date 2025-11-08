ed3005384d319c1a0bfe8074bcb4dcb8b76a0f70
alanawatson <alana.watson9@gmail.com>
2025-11-08 16:01:49 -0500

    added base entity classes

--- src/main/java/entity/Movie.java
+++ src/main/java/entity/Movie.java
@@ -1,4 +1,19 @@
 package entity;
 
 public class Movie {
+
+    /*
+     * TODO: These variables might need to be adjusted based on the API responses
+        * It looks like the response gives a list of genre IDs as ints, and that can
+        * be matched with a string genre description
+     */
+    private String movieID;
+    private String title;
+    private String plot;
+    private List<Integer> genres;
+    private String releaseDate;
+    private double rating;
+    private String posterURL;
+
+    // TODO: constructors, getters/setters
 }
---
+++ src/main/java/entity/WatchedMovie.java
@@ -0,0 +1,11 @@
+package entity;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class WatchedMovie extends Movie {
+
+    private LocalDateTime watchedDate;
+
+    // TODO: constructors, getters/setters
+}
