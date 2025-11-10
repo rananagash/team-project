6aea5bbeddf492acd661e3e626cb228417150a20
oliverbisbee <oliverbisbee@gmail.com>
2025-11-10 16:43:06 -0500

    api read access token implementation

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,4 +1,29 @@
 package app;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public class Main {
+    public static void main(String[] args) throws Exception {
+        // We are going to use the token directly (for now)
+        String token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1ODA5YTFjMmQzNDhjNjI3MDk1ZDllNzY3Y2Y0NWQ2MSIsIm5iZiI6MTc2MjgwODI3Mi4xNDQsInN1YiI6IjY5MTI1MWQwODA0Nzc2ZGJlMmQyNWU2MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.qx1FvwXuldaEQZeXupCmT9FyBjoU3nrGcSSM_hVHFP8";
+
+        // Whenever we end up using an environment variable later, we'll use this
+        // String token = System.getenv("TMDB_API_READ_ACCESS");
+
+        // Calling for the most popular movies
+        String url = "https://api.themoviedb.org/3/movie/popular?language=en-US";
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .header("Authorization", "Bearer " + token)
+                .header("Accept", "application/json")
+                .build();
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.body());
+    }
 }
