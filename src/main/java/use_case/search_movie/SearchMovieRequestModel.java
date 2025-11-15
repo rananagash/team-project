48020174a0391237ab4050d7d2cea6c963e6b36d
chester zhao <chester.zhao@mail.utoronto.ca>
2025-11-15 18:05:40 -0500

    Update Movie constructor and add todos for search

--- src/main/java/use_case/search_movie/SearchMovieRequestModel.java
+++ src/main/java/use_case/search_movie/SearchMovieRequestModel.java
@@ -3,13 +3,20 @@ package use_case.search_movie;
 public class SearchMovieRequestModel {
 
     private final String query;
+    private final int page;
 
-    public SearchMovieRequestModel(String query) {
+    public SearchMovieRequestModel(String query,  int page) {
+
         this.query = query;
+        this.page = 1;
     }
 
     public String getQuery() {
         return query;
+    }
+
+    public int getPage() {
+        return page;
     }
 }
 
