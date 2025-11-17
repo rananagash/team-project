f0a41d1870eccf126d75fd598f025b1ba9dfed10
rananagash <rana.nagash@mail.utoronto.ca>
2025-11-16 22:04:48 -0500

    implemented interface adapter files

--- src/main/java/interface_adapter/view_profile/ViewProfileViewModel.java
+++ src/main/java/interface_adapter/view_profile/ViewProfileViewModel.java
@@ -1,4 +1,15 @@
 package interface_adapter.view_profile;
 
-public class ViewProfileViewModel {
+import interface_adapter.view_login_signup.ViewModel;
+
+/**
+ * View Model for the View Profile View.
+ */
+public class ViewProfileViewModel extends ViewModel<ViewProfileState> {
+    public static final String TITLE_LABEL = "User Profile";
+
+    public ViewProfileViewModel() {
+        super("view profile");
+        setState(new ViewProfileState());
+    }
 }
